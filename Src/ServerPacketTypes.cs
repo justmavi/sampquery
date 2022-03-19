c85ad6a235104203eb2a6f27f8f5ae6d3d3fbf5f
Grisha <36304554+justmavi@users.noreply.github.com>
2022-03-19 20:41:06 +0400

    tab size corrected

--- Src/ServerPacketTypes.cs
+++ Src/ServerPacketTypes.cs
@@ -1,9 +1,10 @@
 namespace SampQueryApi 
 {
-  internal static class ServerPacketTypes {
-    public static char Info { get => 'i'; }
-    public static char Rules { get => 'r'; }     
-    public static char Players { get => 'd'; }
-    public static char Rcon { get => 'x'; }
-  }
+    internal static class ServerPacketTypes 
+    {
+        public static char Info { get => 'i'; }
+        public static char Rules { get => 'r'; }     
+        public static char Players { get => 'd'; }
+        public static char Rcon { get => 'x'; }
+    }
 }
