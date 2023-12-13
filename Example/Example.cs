1768bcb7b6519213382ee7ca2e2ba001610a25c2
J0nathan550 <ihudokormov@gmail.com>
2023-12-13 14:27:01 +0100

    Added supporting of .net 8.0, exposed TimeMilliseconds variable to change the amount of time waiting for the ping. Updated Example.cs.

--- Example/Example.cs
+++ Example/Example.cs
@@ -11,44 +11,80 @@ namespace SampQueryExample
     {
         static async Task Main(string[] args)
         {
+            SampQuery.TimeoutMilliseconds = 1000; // -> If you want the change amount of time of waiting for ping. 
+
             string host = "one.monser.ru";
             ushort port = 7777;
 
             var api = new SampQuery(host, port);
 
-            Console.WriteLine("Information" + Environment.NewLine);
-            ServerInfo serverInfo = await api.GetServerInfoAsync(); // Similar call for synchronus (without await and "Async" postfix)
-            PropertyInfo[] sI_properties = serverInfo.GetType().GetProperties();
-            foreach (PropertyInfo property in sI_properties)
+            try
             {
-                Console.WriteLine($"{property.Name}:\t\t\t{property.GetValue(serverInfo)}");
+                Console.WriteLine("Information" + Environment.NewLine);
+                ServerInfo serverInfo = await api.GetServerInfoAsync(); // Similar call for synchronus (without await and "Async" postfix)
+                PropertyInfo[] sI_properties = serverInfo.GetType().GetProperties();
+                foreach (PropertyInfo property in sI_properties)
+                {
+                    Console.WriteLine($"{property.Name}:\t\t\t{property.GetValue(serverInfo)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Could not Get Server info due server issues or internet issues.");
+                Console.WriteLine("Exception message: " + ex.Message);
             }
 
-            ServerRules sampServerRulesData = await api.GetServerRulesAsync(); // Similar call for synchronus (without await and "Async" postfix)
-
-            Console.WriteLine(Environment.NewLine + "Rules" + Environment.NewLine);
-            PropertyInfo[] sR_properties = sampServerRulesData.GetType().GetProperties();
-            foreach (PropertyInfo property in sR_properties)
+            try
             {
-                Console.WriteLine($"{property.Name}:\t\t\t{property.GetValue(sampServerRulesData)}");
+                ServerRules sampServerRulesData = await api.GetServerRulesAsync(); // Similar call for synchronus (without await and "Async" postfix)
+
+                Console.WriteLine(Environment.NewLine + "Rules" + Environment.NewLine);
+                PropertyInfo[] sR_properties = sampServerRulesData.GetType().GetProperties();
+                foreach (PropertyInfo property in sR_properties)
+                {
+                    Console.WriteLine($"{property.Name}:\t\t\t{property.GetValue(sampServerRulesData)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Could not Get Server Rules due server issues or internet issues.");
+                Console.WriteLine("Exception message: " + ex.Message);
             }
 
-            Console.WriteLine(Environment.NewLine + "Players" + Environment.NewLine);
-
-            IEnumerable<ServerPlayer> serverPlayersInfo = await api.GetServerPlayersAsync(); // Similar call for synchronus (without await and "Async" postfix)
-            Console.WriteLine("ID | Name | Score | Ping\n");
-
-            foreach (ServerPlayer player in serverPlayersInfo)
+            try
             {
-                Console.WriteLine($"{player.PlayerId} | {player.PlayerName} | {player.PlayerScore} | {player.PlayerPing}");
+                Console.WriteLine(Environment.NewLine + "Players" + Environment.NewLine);
+
+                IEnumerable<ServerPlayer> serverPlayersInfo = await api.GetServerPlayersAsync(); // Similar call for synchronus (without await and "Async" postfix)
+                Console.WriteLine("ID | Name | Score | Ping\n");
+
+                foreach (ServerPlayer player in serverPlayersInfo)
+                {
+                    Console.WriteLine($"{player.PlayerId} | {player.PlayerName} | {player.PlayerScore} | {player.PlayerPing}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Could not Get Players info due server issues or internet issues.");
+                Console.WriteLine("Exception message: " + ex.Message);
             }
             Thread.Sleep(3000);
-
             // RCON
-            var server = new SampQuery("localhost", 7777, "hehe");
-
-            string response = await server.SendRconCommandAsync("echo hi"); // Similar call for synchronus (without await and "Async" postfix)
-            Console.WriteLine(response);
+            try
+            {
+                var server = new SampQuery("localhost", 7777, "hehe");
+                string response = await server.SendRconCommandAsync("echo hi"); // Similar call for synchronus (without await and "Async" postfix)
+                Console.WriteLine(response);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Could not Get RCON info due server issues or internet issues.");
+                Console.WriteLine("Exception message: " + ex.Message);
+            }
         }
     }
 }
--- Src/SampQuery.cs
+++ Src/SampQuery.cs
@@ -21,9 +21,13 @@ namespace SAMPQuery
         /// Default SAMP server port (always 7777)
         /// </summary>
         public static readonly ushort DefaultServerPort = 7777;
+        /// <summary>
+        /// The TimeoutMilliseconds variable provides the flexibility to adjust the duration of waiting for a ping response.
+        /// The default recommended value is 5 seconds, equivalent to 5000 milliseconds.
+        /// </summary>
+        public static int TimeoutMilliseconds = 5000;
 
         private readonly int receiveArraySize = 2048;
-        private readonly int timeoutMilliseconds = 5000;
         private readonly IPAddress serverIp;
         private readonly ushort serverPort;
         private readonly string serverIpString;
@@ -135,7 +139,7 @@ namespace SAMPQuery
 
                     var task = this.serverSocket.ReceiveFromAsync(data, SocketFlags.None, rawPoint);
 
-                    if (await Task.WhenAny(task, Task.Delay(this.timeoutMilliseconds)) != task)
+                    if (await Task.WhenAny(task, Task.Delay(TimeoutMilliseconds)) != task)
                     {
                         this.serverSocket.Close();
                         throw new SocketException(10060); // Operation timed out
@@ -152,8 +156,8 @@ namespace SAMPQuery
         {
             this.serverSocket = new Socket(this.serverEndPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp)
             {
-                SendTimeout = this.timeoutMilliseconds,
-                ReceiveTimeout = this.timeoutMilliseconds
+                SendTimeout = TimeoutMilliseconds,
+                ReceiveTimeout = TimeoutMilliseconds
             };
 
             using(var stream = new MemoryStream())
