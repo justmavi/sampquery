35d9834bc08d646adf3e8910415dac163012bd28
justmavi <grisha.poghosyan.dev@gmail.com>
2025-11-23 14:25:13 +0400

    #26 Server connection and data sending parts refactoring

--- Src/Utils/ServerPacketTypes.cs
+++ Src/Utils/ServerPacketTypes.cs
@@ -1,25 +1,28 @@
-namespace SAMPQuery 
+namespace SAMPQuery
 {
     /// <summary>
     /// Server Packet Types. See https://sampwiki.blast.hk/wiki/Query_Mechanism#Recieving_the_packets
     /// </summary>
-    public static class ServerPacketTypes 
+    public static class ServerPacketTypes
     {
         /// <summary>
         /// Information Packet
         /// </summary>
         /// <value>i</value>
         public static char Info => 'i';
+
         /// <summary>
         /// Rule packet
         /// </summary>
         /// <value>r</value>
         public static char Rules => 'r';
+
         /// <summary>
         /// Players packet
         /// </summary>
         /// <value>d</value>    
         public static char Players => 'd';
+
         /// <summary>
         /// RCON packet
         /// </summary>
